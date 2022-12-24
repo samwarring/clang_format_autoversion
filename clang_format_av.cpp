f451e4eb55e302eeeab9e401ecff40fdc38890d6
Sam Warring <samwarring@gmail.com>
2022-12-23 17:54:54 -0800

    Get working on Windows
    - boost process with vcpkg gave me a headache. Just roll my own.
    - Now, there's no support on Linux. I'll do that next.
    - Use `.exe` extension when running on Windows.
    - Fix issue where clang repo always re-runs configure step.
    - NEEDS CLEANUP

--- clang_format_av.cpp
+++ clang_format_av.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <boost/process.hpp>
 #include <fstream>
 #include <string>
 #include <string_view>
 #include <cctype>
 #include <filesystem>
-
+#include <sstream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 constexpr static std::string_view CONFIG_FILE_VERSION_DECLARATION{"# clang-format version "};
 constexpr static std::string_view DUMP_CONFIG_OPTION{"--dump-config"};
@@ -41,11 +43,71 @@ std::string read_version_from_config_file() {
 // Assume the binaries are located next to the current executable.
 std::filesystem::path get_clang_format_binary_path(const std::string& version) {
     std::filesystem::path exe_path;
+    std::string extension;
 #ifdef _WIN32
+    char buffer[2048];
+    DWORD name_size = GetModuleFileNameA(NULL, buffer, 2048);
+    std::string_view full_path{buffer, name_size};
+    exe_path = full_path;
+    extension = ".exe";
 #else
     exe_path = std::filesystem::canonical("/proc/self/exe");
 #endif
-    return exe_path.parent_path() / ("clang-format-av-" + version);
+    return exe_path.parent_path() / ("clang-format-av-" + version + extension);
+}
+
+
+// Executes the process and returns its exit code.
+int execute_child_process(const char* application, const std::vector<std::string> args) {
+    int exit_status = -1;
+    std::ostringstream command_line;
+    command_line << application;
+    for (auto arg : args) {
+        command_line << " \"";
+        
+        // Escape double-quote (") if part of an argument.
+        for (auto ch : arg) {
+            if (ch == '\"') {
+                command_line << "\\\"";
+            }
+            else {
+                command_line << ch;
+            }
+        }
+
+        command_line << "\"";
+    }
+    auto command_line_str = command_line.str();
+#ifdef _WIN32
+    PROCESS_INFORMATION proc_info{0};
+    STARTUPINFOA startup_info{0};
+    startup_info.cb = sizeof(startup_info);
+    if (!CreateProcessA(
+        NULL,
+        command_line_str.data(),
+        NULL,
+        NULL,
+        TRUE, // inherit handles
+        0,
+        NULL,
+        NULL,
+        &startup_info, // startupinfo ptr
+        &proc_info // process information ptr
+    )) {
+        std::cerr << "CreateProcess failed (" << GetLastError() << ')' << std::endl;
+        return exit_status;
+    }
+    WaitForSingleObject(proc_info.hProcess, INFINITE);
+    DWORD dw_exit_status = 0;
+    if (!GetExitCodeProcess(proc_info.hProcess, &dw_exit_status)) {
+        std::cerr << "GetExitCodeProcess failed (" << GetLastError() << ')' << std::endl;
+        return exit_status;
+    }
+    exit_status = static_cast<int>(dw_exit_status);
+#else
+    // TODO: Linux implementation
+#endif
+    return exit_status;
 }
 
 
@@ -59,17 +121,16 @@ int main(int argc, char** argv) {
     // Get path to desired clang-format binary.
     auto clang_format_binary = get_clang_format_binary_path(version);
     if (!std::filesystem::is_regular_file(clang_format_binary)) {
-        std::cerr << "Requested clang-format-" << version << ", but it was not found.\n";
+        std::cerr << "Requested clang-format-av-" << version << ", but it was not found.\n";
         return 1;
     }
 
     // Collect arguments into a vector.
-    std::vector<std::string_view> args;
+    std::vector<std::string> args;
     args.reserve(argc - 1);
     for (int i = 1; i < argc; ++i) {
         args.push_back(argv[i]);
     }
 
-    // Spawn the child process and forward command line arguments.
-    return boost::process::system(clang_format_binary.string(), boost::process::args=args);
+    return execute_child_process(clang_format_binary.string().c_str(), args);
 }
