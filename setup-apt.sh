4785ef5a15e96a0784d19e108e60682c29febcf7
Sam Warring <samwarring@gmail.com>
2023-01-21 00:19:40 -0800

    Use different gcc version depending on ubuntu version
    - Ubuntu 18 does not have gcc-9-arm64, so it
      uses gcc-8-arm64
    - Ubuntu 22 does not have gcc-8-arm64, so it
      uses its default.
    - Ubuntu 20 has both, but uses its default.

--- setup-apt.sh
+++ setup-apt.sh
@@ -5,11 +5,24 @@ wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | 
 echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/kitware.list
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install -y             \
-    ninja-build                \
-    cmake                      \
-    gcc-9                      \
-    g++-9                      \
-    gcc-8-aarch64-linux-gnu    \
-    g++-8-aarch64-linux-gnu    \
+apt-get install -y                 \
+    ninja-build                    \
+    cmake                          \
+    gcc                            \
+    g++                            \
     binutils-aarch64-linux-gnu
+
+UBUNTU_VERSION=$(lsb_release -rs)
+if [ "$UBUNTU_VERSION" = "18.04" ]; then
+    # gcc version on ubuntu 18.04 is too old.
+    apt-get install -y             \
+        gcc-9                      \
+        g++-9                      \
+        gcc-8-aarch64-linux-gnu    \
+        g++-8-aarch64-linux-gnu
+else
+    # Newer Ubuntu versions can stick with default gcc version.
+    apt-get install -y             \
+        gcc-aarch64-linux-gnu      \
+        g++-aarch64-linux-gnu
+fi
